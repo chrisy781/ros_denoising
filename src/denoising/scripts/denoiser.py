705711a6c28900e6631ab8b7d94a48c63d3d7a75
chrisy781 <100926930+chrisy781@users.noreply.github.com>
2023-11-15 11:47:43 +0100

    added improved denoising functionallity

--- src/denoising/scripts/denoiser.py
+++ src/denoising/scripts/denoiser.py
@@ -4,21 +4,102 @@ import pcl
 import sensor_msgs.point_cloud2 as pc2
 import numpy as np
 from sensor_msgs.msg import PointCloud2, PointField
+from scipy.spatial import cKDTree
+from sklearn.linear_model import RANSACRegressor
+from sklearn.linear_model import LinearRegression
 
+def ground_plane_removal(points):
+    # 1) For estimating the ground plane: only regard lowest z value points
+    z_threshold = np.percentile(points[:, 2], 35)  
+    mask = (points[:, 2] < z_threshold) 
+    plane_points = points[mask]
 
-def denoiser(cloud_array):
-    # Extract intensities
-    intensities = cloud_array[:, 3]
+    x = plane_points[:, 0]
+    y = plane_points[:, 1]
+    z = plane_points[:, 2]
 
-    # Calculate the median intensity
-    median_intensity = np.median(intensities)
+    # Reshape x and y to column vectors
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+    X = np.column_stack((x, y))
 
-    # Filter out points with intensities below the median
-    mask = intensities >= median_intensity
-    filtered_cloud_array = cloud_array[mask]
+    # 2) Fit a RANSAC model to the data: for GP estimation
+    ransac = RANSACRegressor(LinearRegression(), random_state=0)
+    ransac.fit(X, z)
 
-    return filtered_cloud_array
+    # Extract the coefficients
+    a, b, d = ransac.estimator_.coef_[0], ransac.estimator_.coef_[1], ransac.estimator_.intercept_
+    
+    # Remove plane
+    x = points[:,0]
+    y = points[:,1]
+    z_adjustment = np.mean(plane_points[:,2]) # plane was too high
+    z = -(a * x + b * y) + z_adjustment # was +d
+    intensity = np.full_like(x, fill_value=255)
+    plane = np.column_stack((x, y, z, intensity))
 
+    # Visualize plane - not applicable
+    num_points = 1000
+    x_p = np.random.uniform(low=-30, high=30, size=num_points)
+    y_p = np.random.uniform(low=-30, high=30, size=num_points)
+    z_p = -(a * x_p + b * y_p) + z_adjustment
+    i_p = np.full_like(x_p, fill_value=255)
+    vis_plane = np.column_stack((x_p, y_p, z_p, i_p))
+
+    # filter out ground plane
+    margin = 0.25  # 10 cm margin
+    mask_z = (np.abs(points[:, 2] - z) < margin) 
+    plane_less_cloud = points[~mask_z]
+
+    return vis_plane, plane_less_cloud
+
+def count_neighbors_inliers(kdtree, query_point, search_radius):
+    # Perform a radius (ball) search
+    neighbors_indices = kdtree.query_ball_point(query_point, search_radius)
+    # Count the total number of neighbors
+    num_neighbors = len(neighbors_indices)
+    return num_neighbors
+
+def denoiser(points): # AGDOR rebuild
+    # --- set params --- 
+    intensity_thresh = 9 
+    alpha = 0.1 
+    angular_res = 0.1 
+    min_neighbors = 5 
+    
+    # --- stage 1 --- intensity filter (should be based labelled data of different dataset - IS NOT ATM!)
+    mask = points[:,3] < intensity_thresh 
+    p_o = points[~mask] 
+    p_i_low = points[mask]
+
+    # --- stage 2 --- adaptive group density outlier removal
+    p_dist = np.linalg.norm(p_i_low, axis=1)
+    SR = alpha*angular_res*p_dist # apply a dynamic search radius
+    print("Median search radius = ", np.median(SR))
+    p_core = []
+    p_outlier = []
+    kdtree = cKDTree(p_i_low[:,0:3])
+
+    for index, (query_point, search_radius) in enumerate(zip(p_i_low, SR)):
+        num_neighbors = count_neighbors_inliers(kdtree, query_point[0:3], search_radius)
+        # print(num_neighbors)
+        if min_neighbors <= num_neighbors: 
+            p_core.append(list(query_point))
+        elif min_neighbors > num_neighbors:
+            p_outlier.append(list(query_point))
+
+    p_outlier = np.array(p_outlier)
+    p_core = np.array(p_core)
+
+    if np.size(p_core)!=0:
+        p_stacked = np.vstack((p_o, p_core))
+        unique_points, indices = np.unique(p_stacked, axis=0, return_index=True)
+        p_o = p_stacked[np.sort(indices)]
+        return p_o
+    else:
+        print("WARNING: the SR did not return any points!")
+        return p_o 
+    
 
 def callback(raw_cloud):
     rospy.loginfo("Processing the point cloud")
@@ -28,7 +109,8 @@ def callback(raw_cloud):
     cloud_array = np.array(list(cloud_data), dtype=np.float32)
 
     # --- applying the denoising method ---
-    filtered_cloud_array = denoiser(cloud_array)
+    _, planeless_cloud = ground_plane_removal(cloud_array)
+    filtered_cloud_array = denoiser(planeless_cloud)
 
     # Setting up the PointCloud2 message which includes the filtered pointcloud data
     filtered_cloud_msg = PointCloud2()
